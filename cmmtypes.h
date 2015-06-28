8c62470345d6d9b98177bc3e14fb2ac21569497f
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2015-06-28 18:20:02 +0800

    add type header

---
+++ cmmtypes.h
@@ -0,0 +1,29 @@
+#ifndef __CMMTYPES_H__
+#define __CMMTYPES_H__
+
+typedef struct Type_* Type;
+typedef struct FieldLidt_* FieldList;
+
+struct Type_{
+    enum{
+        basic,
+        array,
+        structure
+    }kind;
+    union{
+        int basic;
+        struct{
+            Type elem;
+            int size;
+        }array;
+        FieldList structure;
+    }u;
+};
+
+struct FieldLidt_{
+    char* name; //domain name
+    Type type; //domain type
+    FieldList; //next domain
+}
+
+#endif

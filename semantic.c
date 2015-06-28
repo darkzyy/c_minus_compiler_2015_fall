526f11c261ac5482ddd8b189e39fff9da1692ef0
131220070-Zhou Yaoyang <diamondzyy@sina.com>
2015-06-29 01:28:32 +0800

    error type 16 OK

--- semantic.c
+++ semantic.c
@@ -7,9 +7,31 @@
  * and build the ID table
  * for hash table testing
  */
+
+
+
 void build_table(MTnode* root){
     if(root->type == ID){
         add_sym_name(&var_tab,root->str);
+    }
+    switch(root->type){
+        case OptTag:
+            {
+                if(root->children_amount!=0){ // OptTag is not empty
+                    MTnode* struct_id = root->children_list[0];
+                    symbol* s=find_sym(&struct_tab,struct_id->str);
+                    if(s!=NULL){
+                        printf("Error type 16 at Line %d: Duplicated name \"%s\".\n",
+                                    struct_id->location.first_line,struct_id->str);
+                    }
+                    else{
+                        add_sym_name(&struct_tab,struct_id->str);
+                    }
+                }
+                break;
+            }
+        default:{
+                }
     }
     int i;
     for(i=0;i<root->children_amount;i++){
